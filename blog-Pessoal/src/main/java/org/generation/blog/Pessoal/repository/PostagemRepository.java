65de53f8e8d0eda7e013d75dbb3fe3dd926deaa1
unknown <mila_aandrade@hotmail.com>
2020-10-02 12:24:20 -0300

    wip

---
+++ blog-Pessoal/src/main/java/org/generation/blog/Pessoal/repository/PostagemRepository.java
@@ -0,0 +1,15 @@
+package org.generation.blog.Pessoal.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
+import  org.generation.blog.Pessoal.model.Postagem;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {	
+	public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo); 
+
+}
